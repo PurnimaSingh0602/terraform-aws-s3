863494120beb1afbe0736f382430f56d251e6aed
PurnimaSingh0602 <purnima.singh0605@gmail.com>
2026-09-18 15:46:24 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
+      source = "hashicorp/aws"
     }
   }
 }
@@ -16,5 +16,40 @@ resource "aws_s3_bucket" "iac_bucket" {
   tags = {
     Name        = "IaC Demo Bucket"
     Environment = "Lab"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "iac_bucket" {
+  bucket = aws_s3_bucket.iac_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "iac_bucket" {
+  bucket = aws_s3_bucket.iac_bucket.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_s3_bucket_versioning" "iac_bucket" {
+  bucket = aws_s3_bucket.iac_bucket.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_ownership_controls" "iac_bucket" {
+  bucket = aws_s3_bucket.iac_bucket.id
+
+  rule {
+    object_ownership = "BucketOwnerEnforced"
   }
 }
